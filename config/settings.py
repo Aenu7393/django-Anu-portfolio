c6aa8e6a6afa2380b534620db1e9a7dfa0ba7096
natuki <namikin536373@gmail.com>
2026-03-17 19:06:18 +0900

    moziiro siro1

--- config/settings.py
+++ config/settings.py
@@ -121,8 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 #DEBUG = True  # ローカルならTrue
@@ -134,5 +132,5 @@ if DEBUG:
         BASE_DIR / "static",
     ]
 else:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = 'static/'
+    STATIC_ROOT = BASE_DIR / "staticfiles"
